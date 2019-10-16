40ea03e31c4a6aae97894993f8ad649085024f75
黄民根 <2269426123@qq.com>
2019-10-16 15:38:29 +0800

    修改

--- src/cn/smbms/tools/Holle02.java
+++ src/cn/smbms/tools/Holle02.java
@@ -7,5 +7,6 @@ public class Holle02 {
     public static void main(String[] args) {
         System.out.println("456");
         System.out.println("123");
+        System.out.println("122");
     }
 }
